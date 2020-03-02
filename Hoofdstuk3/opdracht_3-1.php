e4190eabb17d3e48461d420656341684338eb24b
siebe <siebe.vanderven1@edu-kw1c.nl>
2020-03-02 11:27:56 +0100

    opdracht 3.1 toegevoegd

--- Hoofdstuk3/opdracht_3-1.php
+++ Hoofdstuk3/opdracht_3-1.php
@@ -1,1 +1,23 @@
 <?php
+    $eventName = "ElfstedenTocht";
+    $eventNameFrisian = "Alvestêdetocht";
+    $lengthTour = 200;
+    $eventType = "schaatstocht";
+    $iceType = "natuurijs";
+    $organizationName = "Koninklijke Vereniging De Friesche Elf Steden";
+    $capitalName = "Leeuwarden";
+    $provinceName = "Friesland";
+    $timesDriven = 15;
+    $firstTour = 1909;
+    $maxDrives = 1;
+    $verhaal = "De ".$eventName." (Fries: ".$eventNameFrisian.") is een ".$lengthTour." kilometer lange ".$eventType." over ".$iceType." die wordt georganiseerd door de ".$organizationName.". ".$capitalName.", de hoofdstad van ".$provinceName.", is start- en aankomstplaats. De".$eventType." is inmiddels ".$timesDriven." maal verreden en werd voor het eerst in ".$firstTour." gereden en wordt maximaal ".$maxDrives." keer per winter gehouden.";
+
+?>
+<?php
+include "../INCLUDES/header.php";
+include "../INCLUDES/aside.php";
+//Tekst hier onder
+echo "<p>".$verhaal."</p>";
+?>
+</body>
+</html>
---
+++ INCLUDES/header.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * User: Siebe van der Ven
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?php echo "Uitwerking van PHP/opdrachten"; ?></title>
+    <link href="/phpopdrachten/CSS/header.css" type="text/css" rel="stylesheet">
+    <link href="/phpopdrachten/CSS/aside.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <a href="/phpopdrachten/index.php"><h1 id="h1"><?php echo "Uitwerking van PHP-opdrachten"; ?></h1></a>
+</header>
