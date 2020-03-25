b28944c2cf1761f886b7acc2324441e5ed4ad41b
siebe <siebe.vanderven1@edu-kw1c.nl>
2020-03-25 16:22:28 +0100

    opdracht 4.4 toegevoegd

--- Hoofdstuk4/opdracht_4-4.php
+++ Hoofdstuk4/opdracht_4-4.php
@@ -1,1 +1,35 @@
 <?php
+include "../INCLUDES/header.php";
+include "../INCLUDES/aside.php";
+//main hieronder
+
+?>
+<?php
+date_default_timezone_set("Europe/Amsterdam");
+
+$task = "";
+
+for($counter = 0; $counter <= 7; $counter++)
+{
+    $date = strtotime("+$counter day");
+    if($counter == 0)
+    {
+        $task = $task . " Dag " . date("w",$date) . " is " . date("l") . " ".date("d-m-Y")."<br>";
+    }
+    elseif($counter == 7)
+    {
+        $task = $task . " Dag " . date("w",$date) . " is " . date("l", $date) . " ".date("d-m-Y",$date)."<br>";
+    }
+    else
+    {
+        $task = $task . " Dag " . date("w",$date) . " is " . date("l", $date) . " ".date("d-m-Y",$date)."<br>";
+    }
+
+}
+echo "<h1>Weekkalender komede week:</h1><br>".$task;
+
+?>
+<?php
+include "../Hoofdstuk3/variabelen.php";
+include "../INCLUDES/footer.php";
+?>
