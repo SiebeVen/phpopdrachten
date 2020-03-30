ba72fcda72323a99e2a5089443c539a702da9f9c
siebe <siebe.vanderven1@edu-kw1c.nl>
2020-03-30 11:45:19 +0200

    lesopdracht53

---
+++ Hoofdstuk5/instructies/lesopdracht53/lesopdracht_functions53.php
@@ -0,0 +1,13 @@
+<?php
+/*************
+ * Maak hier een functie genaamd "getTeacherName" die als return de juiste docent naam mee geeft.
+ **************/
+
+function getTeacherName(){
+    /**Zorg dat je hier de juiste docentnaam met een return terug stuurt.*/
+
+    $courseName = "";
+    return "";
+}
+?>
+
